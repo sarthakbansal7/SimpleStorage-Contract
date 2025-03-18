7a3d1ef6ae2af9769e64fdfb6c5b9221a557a97b
Sarthak Bansal <sarthakbansalog@gmail.com>
2025-03-18 14:25:57 +0530

    Update simple_storage.sol

--- simple_storage.sol
+++ simple_storage.sol
@@ -26,5 +26,8 @@ contract SimpleStorage {
         return myFavoriteNumber;
     }
 
-    
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
